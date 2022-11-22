ba90d5ce955ed8f2591944e5ca0eb1fda70f5a2a
Phil <philipjcowan1@gmail.com>
2022-11-21 19:19:40 -0700

    commit

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,40 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  };
+
+  try{
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
